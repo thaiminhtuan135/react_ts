4a8fa5cdb1ebf412e3e20d3692b352379363c2e9
thaiminhtuan135 <tuana11ht@gmail.com>
2023-05-10 00:44:53 +0700

    use counter

--- src/App.tsx
+++ src/App.tsx
@@ -2,80 +2,38 @@ import React, {useReducer} from 'react';
 import './App.css';
 import useCounter from "./hooks/useCounter";
 
-interface Todo {
-    id: number;
-    task: string;
-    complete: boolean;
-}
-type Action = { type: 'DO_TODO'; id: number } | { type: 'UNDO_TODO'; id: number };
-const initialTodos = [
-    {id: 'a', task: 'Learn React', complete: false,},
-    {id: 'b', task: 'Learn Firebase', complete: false,},
-];
-const todoReducer = (state: Todo[], action: Action): Todo[] => {
-    switch (action.type) {
-        case 'DO_TODO':
-            return state.map(todo => {
-                if (todo.id === action.id) {
-                    return { ...todo, complete: true };
-                } else {
-                    return todo;
-                }
-            });
-        case 'UNDO_TODO':
-            return state.map(todo => {
-                if (todo.id === action.id) {
-                    return { ...todo, complete: false };
-                } else {
-                    return todo;
-                }
-            });
-        default:
+// @ts-ignore
+const countReducer = (state, action) => {
+    switch (action) {
+        case 'INCREMENT' : {
+            return {...state , count: state.count + 1}
+        }
+        case "DECREMENT" : {
+            return {...state , count: state.count - 1}
+        }
+        default :
             return state;
     }
 };
 
+
 function App() {
+    const [state, dispatch] = React.useReducer(countReducer,{count : 0});
 
-    const [todos, dispatch] = React.useReducer<any>(todoReducer, initialTodos);
-    // @ts-ignore
-    const handleChange = (todo) => {
-        // @ts-ignore
-        dispatch({
-            type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
-            id: todo.id,
-        });
-        console.log(todos);
-    };
-    // console.log(todos);
-
-    const {count, increment, decrement, reset, setCount} = useCounter(0);
-    const duplicate = () => {
-        setCount((x : number) => x * 2);
+    const increment = () => {
+        dispatch('INCREMENT')
+        console.log(state)
     }
-    // @ts-ignore
+    const decrement = () => {
+        dispatch('DECREMENT')
+        console.log(state);
+    }
+
     return (
         <div className="App">
-            <div>{count}</div>
-            <button onClick={increment}>increment</button>
-            <button onClick={decrement}>decrement</button>
-            <button onClick={reset}>reset</button>
-            <button onClick={duplicate}>duplicate</button>
-            <ul>
-                {/*@ts-ignore*/}
-                {todos.map(todo => (
-                    <li key={todo.id}>
-                        <label>
-                            <input
-                                type="checkbox"
-                                checked={todo.complete}
-                                onChange={() => handleChange(todo)}
-                            />
-                            {todo.task}
-                        </label>
-                    </li>
-                ))}
-            </ul>
+
+            <button onClick={increment}>increament</button>
+            <button onClick={decrement}>decreament</button>
 
         </div>
     );
